b765b248d36f917ed640b6c52a197ddf67314f6e
ThuThao <thaoptqe170211@fpt.edu.vn>
2024-09-17 09:51:12 +0700

    123

--- src/index.js
+++ src/index.js
@@ -10,6 +10,7 @@ root.render(
     <App />
   </React.StrictMode>
 );
+console.log("Đây là index.js");
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
